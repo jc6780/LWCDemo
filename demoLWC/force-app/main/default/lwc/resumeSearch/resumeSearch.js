b2ec80a02c27253ba301273c0dec919807ed4a85
jc6780 <jcharles0071@gmail.com>
2020-06-16 18:42:05 -0400

    update

--- demoLWC/force-app/main/default/lwc/resumeSearch/resumeSearch.js
+++ demoLWC/force-app/main/default/lwc/resumeSearch/resumeSearch.js
@@ -1,62 +1,25 @@
 import { LightningElement } from 'lwc';
+import { findContacts } from 'data/simpleProvider';
 
-const generateRandomNumber = () => {
-    return Math.round(Math.random() * 100);
-};
+/** The delay used when debouncing event handlers before a method call. */
+const DELAY = 350;
 
-export default class LibsChartjs extends LightningElement {
-    chart;
-    chartjsInitialized = false;
+export default class CompositionContactSearch extends LightningElement {
+    contacts;
+    error;
 
-    config = {
-        type: 'doughnut',
-        data: {
-            datasets: [
-                {
-                    data: [
-                        generateRandomNumber(),
-                        generateRandomNumber(),
-                        generateRandomNumber(),
-                        generateRandomNumber(),
-                        generateRandomNumber()
-                    ],
-                    backgroundColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(255, 159, 64)',
-                        'rgb(255, 205, 86)',
-                        'rgb(75, 192, 192)',
-                        'rgb(54, 162, 235)'
-                    ],
-                    label: 'Dataset 1'
-                }
-            ],
-            labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue']
-        },
-        options: {
-            responsive: true,
-            legend: {
-                position: 'right'
-            },
-            animation: {
-                animateScale: true,
-                animateRotate: true
+    handleKeyChange(event) {
+        // Debouncing this method: Do not actually invoke the method call as long as this function is
+        // being called within a delay of DELAY.
+        window.clearTimeout(this.delayTimeout);
+        const searchKey = event.target.value;
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        this.delayTimeout = setTimeout(() => {
+            try {
+                this.contacts = findContacts(searchKey);
+            } catch (e) {
+                this.error = e;
             }
-        }
-    };
-
-    renderedCallback() {
-        if (this.chartjsInitialized) {
-            return;
-        }
-        this.chartjsInitialized = true;
-        this.loadChartJs();
-    }
-
-    async loadChartJs() {
-        await require(/* webpackChunkName: "chartJs" */ 'chart.js');
-        const ctx = this.template
-            .querySelector('canvas.donut')
-            .getContext('2d');
-        this.chart = new window.Chart(ctx, this.config);
+        }, DELAY);
     }
 }
